e4b2d71e1b6c24ae997595338271ac2cbfef48be
Alex Curran <alexcurran.va@gmail.com>
2021-06-25 12:55:23 -0500

    More server setup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,34 @@
 "use strict";
 
+require("dotenv").config();
+
 const express = require("express");
+const path = require("path");
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
+if (process.env.NODE_ENV === "development") {
+    const morgan = require("morgan");
+
+    app.use(morgan("dev"));
+}
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+// General server error handling
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    return res.status(500).json({ error: err.message });
+});
+
+// Catch-all GET handler to send back React's index.html file
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode, and listening on port ${PORT}.`));
