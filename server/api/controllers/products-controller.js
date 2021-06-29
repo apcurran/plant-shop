f9809b491559671c5e8c63fea86ce20f64709e84
Alex Curran <alexcurran.va@gmail.com>
2021-06-29 14:08:26 -0500

    Adjust query for price of one gallon plant

--- server/api/controllers/products-controller.js
+++ server/api/controllers/products-controller.js
@@ -21,6 +21,7 @@ async function getProducts(req, res, next) {
                 ON product.product_id = product_img.product_id
             INNER JOIN product_extra_info
                 ON product.product_id = product_extra_info.product_id
+                AND product_extra_info.size = 1
             ORDER BY product.category
         `)).rows;
         console.log(products);
