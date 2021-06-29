a6d75677ae29bf0c595d5514d774070ded5a8f64
Alex Curran <alexcurran.va@gmail.com>
2021-06-29 13:12:24 -0500

    Update getProducts query to join 3 tables

--- server/api/controllers/products-controller.js
+++ server/api/controllers/products-controller.js
@@ -13,12 +13,17 @@ async function getProducts(req, res, next) {
                 product_img.url,
                 product_img.alt_text,
                 product_img.width,
-                product_img.height
+                product_img.height,
+
+                product_extra_info.price
             FROM product
             INNER JOIN product_img
                 ON product.product_id = product_img.product_id
+            INNER JOIN product_extra_info
+                ON product.product_id = product_extra_info.product_id
             ORDER BY product.category
         `)).rows;
+        console.log(products);
 
     } catch (err) {
         next(err);
