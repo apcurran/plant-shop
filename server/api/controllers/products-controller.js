2e5be4374d0ed7b359fa940dd553c8e71f179254
Alex Curran <alexcurran.va@gmail.com>
2021-07-01 11:23:00 -0500

    Remove console.log

--- server/api/controllers/products-controller.js
+++ server/api/controllers/products-controller.js
@@ -61,7 +61,6 @@ async function getProduct(req, res, next) {
             `,
             [productId]
         )).rows;
-        console.log(product);
         
         const formattedProduct = transformProductResults(product);
 
