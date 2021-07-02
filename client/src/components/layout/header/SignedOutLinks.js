601a74e8487dc1c24fe8ec77b96ca23d3d1d9f7d
Alex Curran <alexcurran.va@gmail.com>
2021-07-02 10:59:42 -0500

    Add header styling

--- client/src/components/layout/header/SignedOutLinks.js
+++ client/src/components/layout/header/SignedOutLinks.js
@@ -4,10 +4,10 @@ function SignedOutLinks() {
     return (
         <ul className="nav__links-list nav__links-list--auth">
             <li className="nav__item">
-                <NavLink to="/auth/log-in">Log In</NavLink>
+                <NavLink to="/auth/log-in" className="nav__link">Log In</NavLink>
             </li>
             <li className="nav__item">
-                <NavLink to="/auth/sign-up">Sign Up</NavLink>
+                <NavLink to="/auth/sign-up" className="nav__link">Sign Up</NavLink>
             </li>
         </ul>
     );
