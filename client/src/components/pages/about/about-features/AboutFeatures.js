7e26094cce40df6a692847eeb0e94a3a0225c068
Alex Curran <alexcurran.va@gmail.com>
2021-07-08 13:41:26 -0500

    Video with autoplay

--- client/src/components/pages/about/about-features/AboutFeatures.js
+++ client/src/components/pages/about/about-features/AboutFeatures.js
@@ -1,4 +1,4 @@
-import { Image, Transformation } from "cloudinary-react";
+import { Image, Video, Transformation } from "cloudinary-react";
 
 import "./AboutFeatures.css";
 
@@ -6,8 +6,8 @@ function AboutFeatures() {
     return (
         <section className="about__features">
             <figure className="about__features__fig">
-                <Image className="about__features__fig__img" publicId="evergreen-app/living-room-table-plants_dv16lm.jpg" loading="lazy">
-                    <Transformation />
+                <Image className="about__features__fig__img" publicId="evergreen-app/living-room-table-plants_dv16lm.jpg" width="1920" height="1440" loading="lazy">
+                    <Transformation width="600" crop="fill" quality="auto" fetchFormat="auto" />
                 </Image>
             </figure>
             <article className="about__features__article">
@@ -16,6 +16,11 @@ function AboutFeatures() {
                     Aenean eget elit nisl. Maecenas ac semper sapien. Vestibulum orci orci, luctus vel est sit amet, gravida ullamcorper sem. Nullam lobortis scelerisque pulvinar. Aliquam id libero quis quam ultrices suscipit quis vitae ligula. Etiam gravida porta ultricies. Sed elementum venenatis felis, ut elementum leo eleifend non. Aliquam sem tortor, egestas consectetur urna vel, molestie dapibus sem.
                 </p>
             </article>
+            <div className="about__features__video-container">
+                <Video className="about__features__video" publicId="evergreen-app/potted-plant-blowing-in-wind_gb7lya" loop="true" autoplay="true" muted="muted" fetchFormat="auto" quality="auto">
+
+                </Video>
+            </div>
         </section>
     );
 }
