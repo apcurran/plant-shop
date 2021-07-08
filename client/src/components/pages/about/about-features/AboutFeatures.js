3edc0e1029230ea217c67198e2eec46c5617c805
Alex Curran <alexcurran.va@gmail.com>
2021-07-08 14:18:28 -0500

    Finish layout/styling for About comp page

--- client/src/components/pages/about/about-features/AboutFeatures.js
+++ client/src/components/pages/about/about-features/AboutFeatures.js
@@ -17,10 +17,26 @@ function AboutFeatures() {
                 </p>
             </article>
             <div className="about__features__video-container">
-                <Video className="about__features__video" publicId="evergreen-app/potted-plant-blowing-in-wind_gb7lya" loop="true" autoplay="true" muted="muted" fetchFormat="auto" quality="auto">
-
+                <Video className="about__features__video" publicId="evergreen-app/potted-plant-blowing-in-wind_gb7lya" loop="true" autoplay="true" muted="muted">
+                    <Transformation width="1300" height="600" crop="fill" quality="auto" fetchFormat="auto" />
                 </Video>
             </div>
+            <article className="about__features__article">
+                <h2 className="about__features__article__title">Food From Your Backyard</h2>
+                <p className="about__features__article__desc">
+                    Quisque semper condimentum ex, at luctus sem gravida et. Aliquam sodales sem eu nibh iaculis aliquet. Sed nibh erat, pellentesque malesuada interdum tempor, aliquet nec arcu. In vel venenatis ante. Suspendisse non pellentesque nulla. Etiam congue sed mauris vel molestie.
+                </p>
+            </article>
+            <figure className="about__features__fig about__features__fig--right">
+                <Image className="about__features__fig__img" publicId="evergreen-app/three-pears_pvexbh.jpg" width="1920" height="1228" loading="lazy">
+                    <Transformation width="600" crop="fill" quality="auto" fetchFormat="auto" />
+                </Image>
+            </figure>
+            <figure className="about__features__fig about__features__fig--span-all">
+                <Image className="about__features__fig__img" publicId="evergreen-app/light-through-leaves_sllhnh.jpg" width="1920" height="1282" loading="lazy">
+                    <Transformation width="1300" height="600" crop="fill" quality="auto" fetchFormat="auto" />
+                </Image>
+            </figure>
         </section>
     );
 }
