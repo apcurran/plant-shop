f80043e5e418dd0a8f7a2031d76d14529ea63856
Alex Curran <alexcurran.va@gmail.com>
2021-07-08 11:17:31 -0500

    AboutHero comp creation and addition

--- client/src/components/pages/about/About.js
+++ client/src/components/pages/about/About.js
@@ -1,11 +1,13 @@
 import "./About.css";
 import Header from "../../layout/header/Header";
+import AboutHero from "./about-hero/AboutHero";
 
 function About() {
     return (
         <div className="about">
             <div className="about-grid-wrapper">
                 <Header />
+                <AboutHero />
             </div>
         </div>
     );
---
+++ client/src/components/pages/about/about-hero/AboutHero.js
@@ -0,0 +1,14 @@
+import "./AboutHero.css";
+
+function AboutHero() {
+    return (
+        <section className="about__hero">
+            <div className="about__hero__card">
+                <h2 className="about__hero__card__title">Our Mission</h2>
+                <p className="about__hero__card__desc">We're here to inspire a green, healthy world for all.</p>
+            </div>
+        </section>
+    );
+}
+
+export default AboutHero;
