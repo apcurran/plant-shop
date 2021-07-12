b25ea06c64cd7eed746277b9ea3963f1b90a9163
Alex Curran <alexcurran.va@gmail.com>
2021-07-12 14:18:05 -0500

    CollectionNav styling for links

--- client/src/components/pages/shop/collection-nav/CollectionNav.js
+++ client/src/components/pages/shop/collection-nav/CollectionNav.js
@@ -7,16 +7,16 @@ function CollectionNav() {
         <nav className="shop__collection-nav">
             <ul className="shop__collection-nav__list">
                 <li className="shop__collection-nav__list__item">
-                    <NavLink exact to="/collections">All Plants</NavLink>
+                    <NavLink exact to="/collections" className="shop__collection-nav__list__link">All Plants</NavLink>
                 </li>
                 <li className="shop__collection-nav__list__item">
-                    <NavLink to="/collections/house-plants">House Plants</NavLink>
+                    <NavLink to="/collections/house-plants" className="shop__collection-nav__list__link">House Plants</NavLink>
                 </li>
                 <li className="shop__collection-nav__list__item">
-                    <NavLink to="/collections/fruit-trees">Fruit Trees</NavLink>
+                    <NavLink to="/collections/fruit-trees" className="shop__collection-nav__list__link">Fruit Trees</NavLink>
                 </li>
                 <li className="shop__collection-nav__list__item">
-                    <NavLink to="/collections/shade-trees">Shade Trees</NavLink>
+                    <NavLink to="/collections/shade-trees" className="shop__collection-nav__list__link">Shade Trees</NavLink>
                 </li>
             </ul>
         </nav>
