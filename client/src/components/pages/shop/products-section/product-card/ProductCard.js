5395da7505f2ffb03660c69058d107e6c3cf1c92
Alex Curran <alexcurran.va@gmail.com>
2021-07-14 09:25:14 -0500

    Add link to cards

--- client/src/components/pages/shop/products-section/product-card/ProductCard.js
+++ client/src/components/pages/shop/products-section/product-card/ProductCard.js
@@ -1,20 +1,23 @@
+import { Link } from "react-router-dom";
 import { Image, Transformation } from "cloudinary-react";
 
 import "./ProductCard.css";
 
 function ProductCard({ productData }) {
     return (
-        <article className="shop__products-section__card">
-            <figure className="shop__products-section__card__fig">
-                <Image publicId={productData.publicId} alt={productData.altText} className="shop__products-section__card__img" loading="lazy" width={productData.width} height={productData.height}>
-                    <Transformation width="400" height="600" crop="fill" quality="auto" fetchFormat="auto" />
-                </Image>
-            </figure>
-            <div className="shop__products-section__card__content-group">
-                <h3 className="shop__products-section__card__title">{productData.title}</h3>
-                <span className="shop__products-section__card__price">${productData.price}</span>
-            </div>
-        </article>
+        <Link to={{pathname: `/collections/${productData.productId}`}}>
+            <article className="shop__products-section__card">
+                <figure className="shop__products-section__card__fig">
+                    <Image publicId={productData.publicId} alt={productData.altText} className="shop__products-section__card__img" loading="lazy" width={productData.width} height={productData.height}>
+                        <Transformation width="400" height="600" crop="fill" quality="auto" fetchFormat="auto" />
+                    </Image>
+                </figure>
+                <div className="shop__products-section__card__content-group">
+                    <h3 className="shop__products-section__card__title">{productData.title}</h3>
+                    <span className="shop__products-section__card__price">${productData.price}</span>
+                </div>
+            </article>
+        </Link>
     );
 }
 
