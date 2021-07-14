14b229ba2338e40dbc24268bc48a2986cfd9b321
Alex Curran <alexcurran.va@gmail.com>
2021-07-14 15:24:07 -0500

    Product info articles

--- client/src/components/pages/product/Product.js
+++ client/src/components/pages/product/Product.js
@@ -48,6 +48,21 @@ function Product() {
                         </div>
                         <h3 className="product__content__size-title">Size</h3>
                         {prodBtns}
+                        <div className="product__content__add-info-container">
+                            <button className="product__content__add-btn">Add to Cart</button>
+                            <span className="product__content__selected-price">Selected Price Here</span>
+                        </div>
+                        <p className="product__content__desc">{productData.description}</p>
+                        <div className="product__content__info-article-container">
+                            <article className="product__content__info-article product__content__shipping-info">
+                                <h3 className="product__content__shipping-info__title">Free Shipping</h3>
+                                <p className="product__content__shipping-info__desc">for all orders.</p>
+                            </article>
+                            <article className="product__content__info-article product__content__warranty-info">
+                                <h3 className="product__content__warranty-info__title">One Year Warranty</h3>
+                                <p className="product__content__warranty-info__desc">No hassle warranty against defects.</p>
+                            </article>
+                        </div>
                     </section>
                 </div>
             </main>
