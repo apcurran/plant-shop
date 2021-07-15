b747835ed734386b6d8687ede86a76cd0f2f8d43
Alex Curran <alexcurran.va@gmail.com>
2021-07-15 14:07:28 -0500

    Auth validation

---
+++ server/api/validation/auth-validation.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const Joi = require("joi");
+
+function signupValidation(data) {
+    const schema = Joi.object({
+        firstName: Joi
+                    .string()
+                    .max(50)
+                    .required(),
+        lastName:  Joi
+                    .string()
+                    .max(50)
+                    .required(),
+        email:     Joi
+                    .string()
+                    .email()
+                    .max(100)
+                    .required(),
+        password:  Joi
+                    .string()
+                    .min(6)
+                    .max(50)
+                    .required(),
+        isAdmin:   Joi
+                    .boolean()
+    });
+
+    return schema.validateAsync(data);
+}
+
+function loginValidation(data) {
+    const schema = Joi.object({
+        email:     Joi
+                    .string()
+                    .email()
+                    .max(100)
+                    .required(),
+        password:  Joi
+                    .string()
+                    .min(6)
+                    .max(50)
+                    .required()
+    });
+
+    return schema.validateAsync(data);
+}
+
+module.exports = {
+    signupValidation,
+    loginValidation
+};
