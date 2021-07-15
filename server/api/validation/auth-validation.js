897e2495b5b5206a9596a54473c7609eba903d28
Alex Curran <alexcurran.va@gmail.com>
2021-07-15 16:06:11 -0500

    Exchange isAdmin Boolean for adminPassword String field

--- server/api/validation/auth-validation.js
+++ server/api/validation/auth-validation.js
@@ -4,26 +4,26 @@ const Joi = require("joi");
 
 function signupValidation(data) {
     const schema = Joi.object({
-        firstName: Joi
-                    .string()
-                    .max(50)
-                    .required(),
-        lastName:  Joi
-                    .string()
-                    .max(50)
-                    .required(),
-        email:     Joi
-                    .string()
-                    .email()
-                    .max(100)
-                    .required(),
-        password:  Joi
-                    .string()
-                    .min(6)
-                    .max(50)
-                    .required(),
-        isAdmin:   Joi
-                    .boolean()
+        firstName:      Joi
+                            .string()
+                            .max(50)
+                            .required(),
+        lastName:       Joi
+                            .string()
+                            .max(50)
+                            .required(),
+        email:          Joi
+                            .string()
+                            .email()
+                            .max(100)
+                            .required(),
+        password:       Joi
+                            .string()
+                            .min(6)
+                            .max(50)
+                            .required(),
+        adminPassword:   Joi
+                            .string()
     });
 
     return schema.validateAsync(data);
