2c3f7ac4ace400f59afc53f36f483378917ae6b2
Alex Curran <alexcurran.va@gmail.com>
2021-07-15 16:08:48 -0500

    Implement sign up and log in validation

--- server/api/controllers/auth-controller.js
+++ server/api/controllers/auth-controller.js
@@ -4,10 +4,17 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const db = require("../../db/index");
+const { signupValidation, loginValidation } = require("../validation/auth-validation");
 
 // POST controllers
 async function postSignup(req, res, next) {
     // TODO: Validate incoming data first
+    try {
+        await signupValidation(req.body);
+
+    } catch (err) {
+        return res.status(400).json({ error: err.details[0].message });
+    }
 
     try {
         
@@ -17,6 +24,14 @@ async function postSignup(req, res, next) {
 }
 
 async function postLogin(req, res, next) {
+    // TODO: Validate incoming data first
+    try {
+        await loginValidation(req.body);
+
+    } catch (err) {
+        return res.status(400).json({ error: err.details[0].message });
+    }
+
     try {
         
     } catch (err) {
