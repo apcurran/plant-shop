b9cf6be29a3c23198afc5a109566e96f6ee2c771
Alex Curran <alexcurran.va@gmail.com>
2021-07-20 13:34:03 -0500

    Form styling

--- client/src/components/pages/auth/sign-up/sign-up-card/sign-up-form/SignUpForm.js
+++ client/src/components/pages/auth/sign-up/sign-up-card/sign-up-form/SignUpForm.js
@@ -14,15 +14,15 @@ function SignUpForm() {
                 <label htmlFor="first-name" className="auth-card__content__form__label sign-up__card__content__form__label">First Name</label>
                 <input onChange={(event) => setFirstName(event.target.value)} type="text" name="first-name" id="first-name" className="auth-card__content__form__input sign-up__content__form__input" required />
             </div>
-            <div className="sign-up__card__content__form-group">
+            <div className="auth-card__content__form-group sign-up__card__content__form-group">
                 <label htmlFor="last-name" className="auth-card__content__form__label sign-up__card__content__form__label">Last Name</label>
                 <input onChange={(event) => setLastName(event.target.value)} type="text" name="last-name" id="last-name" className="auth-card__content__form__input sign-up__content__form__input" required />
             </div>
-            <div className="sign-up__card__content__form-group">
+            <div className="auth-card__content__form-group sign-up__card__content__form-group">
                 <label htmlFor="email" className="auth-card__content__form__label sign-up__card__content__form__label">Email</label>
                 <input onChange={(event) => setEmail(event.target.value)} type="email" name="email" id="email" className="auth-card__content__form__input sign-up__content__form__input" required />
             </div>
-            <div className="sign-up__card__content__form-group">
+            <div className="auth-card__content__form-group sign-up__card__content__form-group">
                 <label htmlFor="password" className="auth-card__content__form__label sign-up__card__content__form__label">Password</label>
                 <input onChange={(event) => setPassword(event.target.value)} type="password" name="password" id="password" className="auth-card__content__form__input sign-up__content__form__input" required />
             </div>
