aa889b90f142e91839c249e14e53883e23aa3497
Alex Curran <alexcurran.va@gmail.com>
2021-07-21 10:46:53 -0500

    Remove unused CSS classes

--- client/src/components/pages/auth/auth-card/sign-up-form/SignUpForm.js
+++ client/src/components/pages/auth/auth-card/sign-up-form/SignUpForm.js
@@ -46,24 +46,24 @@ function SignUpForm() {
     }
 
     return (
-        <form onSubmit={handleSubmit} className="auth-card__content__form sign-up__card__content__form">
-            <div className="auth-card__content__form-group sign-up__card__content__form-group">
-                <label htmlFor="first-name" className="auth-card__content__form__label sign-up__card__content__form__label">First Name</label>
-                <input onChange={(event) => setFirstName(event.target.value)} type="text" name="first-name" id="first-name" className="auth-card__content__form__input sign-up__content__form__input" required />
+        <form onSubmit={handleSubmit} className="auth-card__content__form">
+            <div className="auth-card__content__form-group">
+                <label htmlFor="first-name" className="auth-card__content__form__label">First Name</label>
+                <input onChange={(event) => setFirstName(event.target.value)} type="text" name="first-name" id="first-name" className="auth-card__content__form__input" required />
             </div>
-            <div className="auth-card__content__form-group sign-up__card__content__form-group">
-                <label htmlFor="last-name" className="auth-card__content__form__label sign-up__card__content__form__label">Last Name</label>
-                <input onChange={(event) => setLastName(event.target.value)} type="text" name="last-name" id="last-name" className="auth-card__content__form__input sign-up__content__form__input" required />
+            <div className="auth-card__content__form-group">
+                <label htmlFor="last-name" className="auth-card__content__form__label">Last Name</label>
+                <input onChange={(event) => setLastName(event.target.value)} type="text" name="last-name" id="last-name" className="auth-card__content__form__input" required />
             </div>
-            <div className="auth-card__content__form-group sign-up__card__content__form-group">
-                <label htmlFor="email" className="auth-card__content__form__label sign-up__card__content__form__label">Email</label>
-                <input onChange={(event) => setEmail(event.target.value)} type="email" name="email" id="email" className="auth-card__content__form__input sign-up__content__form__input" required />
+            <div className="auth-card__content__form-group">
+                <label htmlFor="email" className="auth-card__content__form__label">Email</label>
+                <input onChange={(event) => setEmail(event.target.value)} type="email" name="email" id="email" className="auth-card__content__form__input" required />
             </div>
-            <div className="auth-card__content__form-group sign-up__card__content__form-group">
-                <label htmlFor="password" className="auth-card__content__form__label sign-up__card__content__form__label">Password (6 character minimum)</label>
-                <input onChange={(event) => setPassword(event.target.value)} type="password" name="password" id="password" className="auth-card__content__form__input sign-up__content__form__input" min="6" max="50" required />
+            <div className="auth-card__content__form-group">
+                <label htmlFor="password" className="auth-card__content__form__label">Password (6 character minimum)</label>
+                <input onChange={(event) => setPassword(event.target.value)} type="password" name="password" id="password" className="auth-card__content__form__input" min="6" max="50" required />
             </div>
-            <button className="auth-card__content__form__submit-btn sign-up__card__content__submit-btn">Submit</button>
+            <button className="auth-card__content__form__submit-btn">Submit</button>
             {error ? <ErrorMsg msg={error} /> : null}
         </form>
     );
