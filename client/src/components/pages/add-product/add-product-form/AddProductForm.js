4e545daf1d25b400229c1044efa459e887a97790
Alex Curran <alexcurran.va@gmail.com>
2021-07-28 12:24:12 -0500

    AddProductForm layout

--- client/src/components/pages/add-product/add-product-form/AddProductForm.js
+++ client/src/components/pages/add-product/add-product-form/AddProductForm.js
@@ -1,3 +1,4 @@
+import "./AddProductForm.css";
 import FormSegment from "../../../ui/form-segment/FormSegment";
 import NumberBadge from "../../../ui/number-badge/NumberBadge";
 
@@ -5,14 +6,31 @@ function AddProductForm() {
     return (
         <form className="add-product__form">
             <FormSegment>
-                <NumberBadge>1</NumberBadge>
-                <h2>Hi</h2>
+                <div className="add-product__form__column--left">
+                    <NumberBadge>1</NumberBadge>
+                    <h2>Hi</h2>
+                </div>
+                <div className="add-product__form__column--right">
+                    <h3>Title</h3>
+                </div>
             </FormSegment>
             <FormSegment>
-                <h2>Hi</h2>
+                <div className="add-product__form__column--left">
+                    <NumberBadge>2</NumberBadge>
+                    <h2>Hi</h2>
+                </div>
+                <div className="add-product__form__column--right">
+                    <h3>Title</h3>
+                </div>
             </FormSegment>
             <FormSegment>
-                <h2>Hi</h2>
+                <div className="add-product__form__column--left">
+                    <NumberBadge>3</NumberBadge>
+                    <h2>Hi</h2>
+                </div>
+                <div className="add-product__form__column--right">
+                    <h3>Title</h3>
+                </div>
             </FormSegment>
         </form>
     );
