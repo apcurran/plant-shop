4ef4bd95fc2dc8cedbbf2d579b84028112e853bd
Alex Curran <alexcurran.va@gmail.com>
2021-07-28 15:22:20 -0500

    Base onSubmit handler and enctype val added to form elem

--- client/src/components/pages/add-product/add-product-form/AddProductForm.js
+++ client/src/components/pages/add-product/add-product-form/AddProductForm.js
@@ -5,14 +5,28 @@ import FormSegment from "../../../ui/form-segment/FormSegment";
 import NumberBadge from "../../../ui/number-badge/NumberBadge";
 
 function AddProductForm() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
     const [category, setCategory] = useState("house plants");
 
-    function handleSelect(event) {
+    function handleTitleChange(event) {
+        setTitle(event.target.value);
+    }
+
+    function handleDescriptionChange(event) {
+        setDescription(event.target.value);
+    }
+
+    function handleSelectChange(event) {
         setCategory(event.target.value);
     }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        <form className="add-product__form">
+        <form onSubmit={handleSubmit} className="add-product__form" enctype="multipart/form-data">
             <FormSegment>
                 <div className="add-product__form__column--left">
                     <NumberBadge>1</NumberBadge>
@@ -21,15 +35,15 @@ function AddProductForm() {
                 <div className="add-product__form__column--right">
                     <div className="form-group">
                         <label htmlFor="title" className="form-group__label">Title</label>
-                        <input id="title" type="text" className="form-group__input" />
+                        <input onChange={handleTitleChange} id="title" type="text" className="form-group__input" />
                     </div>
                     <div className="form-group">
                         <label htmlFor="description" className="form-group__label">Description</label>
-                        <input id="description" type="text" className="form-group__input" />
+                        <textarea onChange={handleDescriptionChange} id="description" rows="10" className="form-group__textarea"></textarea>
                     </div>
                     <div className="form-group">
                         <label htmlFor="category" className="form-group__label">Category</label>
-                        <select value={category} onChange={handleSelect} id="category" className="form-group__select">
+                        <select value={category} onChange={handleSelectChange} id="category" className="form-group__select">
                             <option value="house plants">House Plant</option>
                             <option value="fruit trees">Fruit Tree</option>
                             <option value="shade trees">Shade Tree</option>
