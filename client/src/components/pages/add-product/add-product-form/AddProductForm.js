b29fdd1910853872269c74bf1e3f9b04b8a02073
Alex Curran <alexcurran.va@gmail.com>
2021-07-28 15:54:53 -0500

    Product Sizes section data for form

--- client/src/components/pages/add-product/add-product-form/AddProductForm.js
+++ client/src/components/pages/add-product/add-product-form/AddProductForm.js
@@ -7,6 +7,12 @@ import NumberBadge from "../../../ui/number-badge/NumberBadge";
 function AddProductForm() {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [smallAmt, setSmallAmt] = useState(0);
+    const [smallPrice, setSmallPrice] = useState(0);
+    const [medAmt, setMedAmt] = useState(0);
+    const [medPrice, setMedPrice] = useState(0);
+    const [lgAmt, setLgAmt] = useState(0);
+    const [lgPrice, setLgPrice] = useState(0);
     const [category, setCategory] = useState("house plants");
 
     function handleTitleChange(event) {
@@ -19,6 +25,33 @@ function AddProductForm() {
 
     function handleSelectChange(event) {
         setCategory(event.target.value);
+    }
+
+    // Small
+    function handleSmallAmtChange(event) {
+        setSmallAmt(event.target.value);
+    }
+
+    function handleSmallPriceChange(event) {
+        setSmallPrice(event.target.value);
+    }
+
+    // Medium
+    function handleMediumAmtChange(event) {
+        setMedAmt(event.target.value);
+    }
+
+    function handleMediumPriceChange(event) {
+        setMedPrice(event.target.value);
+    }
+
+    // Large
+    function handleLargeAmtChange(event) {
+        setLgAmt(event.target.value);
+    }
+
+    function handleLargePriceChange(event) {
+        setLgPrice(event.target.value);
     }
 
     function handleSubmit(event) {
@@ -57,7 +90,39 @@ function AddProductForm() {
                     <h2>Product Sizes</h2>
                 </div>
                 <div className="add-product__form__column--right">
-                    <h3>Title</h3>
+                    <div className="add-product__form__sizes-group">
+                        <h3 className="add-product__form__sub-heading">Size Small</h3>
+                        <div className="form-group">
+                            <label htmlFor="size-small-amt" className="form-group__label">Gallon Amount</label>
+                            <input onChange={handleSmallAmtChange} id="size-small-amt" type="number" className="form-group__input" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="size-small-price" className="form-group__label">Price</label>
+                            <input onChange={handleSmallPriceChange} id="size-small-price" type="number" className="form-group__input" />
+                        </div>
+                    </div>
+                    <div className="add-product__form__sizes-group">
+                        <h3 className="add-product__form__sub-heading">Size Medium</h3>
+                        <div className="form-group">
+                            <label htmlFor="size-medium-amt" className="form-group__label">Gallon Amount</label>
+                            <input onChange={handleMediumAmtChange} id="size-medium-amt" type="number" className="form-group__input" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="size-medium-price" className="form-group__label">Price</label>
+                            <input onChange={handleMediumPriceChange} id="size-medium-price" type="number" className="form-group__input" />
+                        </div>
+                    </div>
+                    <div className="add-product__form__sizes-group">
+                        <h3 className="add-product__form__sub-heading">Size Large</h3>
+                        <div className="form-group">
+                            <label htmlFor="size-large-amt" className="form-group__label">Gallon Amount</label>
+                            <input onChange={handleLargeAmtChange} id="size-large-amt" type="number" className="form-group__input" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="size-large-price" className="form-group__label">Price</label>
+                            <input onChange={handleLargePriceChange} id="size-large-price" type="number" className="form-group__input" />
+                        </div>
+                    </div>
                 </div>
             </FormSegment>
             <FormSegment>
