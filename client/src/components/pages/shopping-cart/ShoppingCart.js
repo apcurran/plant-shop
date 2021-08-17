f1896e6fa8f8b5b83f79cc1b242a33450364824a
Alex Curran <alexcurran.va@gmail.com>
2021-08-17 15:05:40 -0500

    General table structure (some temp hard-coding) and layout

--- client/src/components/pages/shopping-cart/ShoppingCart.js
+++ client/src/components/pages/shopping-cart/ShoppingCart.js
@@ -1,7 +1,16 @@
+import Header from "../../layout/header/Header";
+import TitleBar from "../../ui/title-bar/TitleBar";
+import MainWrapper from "../../layout/main-wrapper/MainWrapper";
+import CartTable from "./cart-table/CartTable";
+
 function ShoppingCart() {
     return (
-        <div>
-            Shopping Cart
+        <div className="cart-page">
+            <Header />
+            <MainWrapper>
+                <TitleBar>Shopping Cart</TitleBar>
+                <CartTable />
+            </MainWrapper>
         </div>
     );
 }
---
+++ client/src/components/pages/shopping-cart/cart-table/CartTable.js
@@ -0,0 +1,56 @@
+import "./CartTable.css";
+
+function CartTable() {
+    return (
+        <table className="cart-table">
+            <thead className="cart-table__thead">
+                <tr className="cart-table__thead__tr">
+                    <th className="cart-table__thead__tr__th">Product</th>
+                    <th className="cart-table__thead__tr__th">Price</th>
+                    <th className="cart-table__thead__tr__th">Quantity</th>
+                    <th className="cart-table__thead__tr__th">Total</th>
+                </tr>
+            </thead>
+            <tbody className="cart-table__tbody">
+                <tr className="cart-table__tbody__tr">
+                    <td className="cart-table__tbody__tr__td cart-table__prod-info">
+                        <figure className="cart-table__prod-info__fig">
+                            img here
+                        </figure>
+                        <div className="cart-table__prod-info__desc-container">
+                            <h2 className="cart-table__prod-info__title">Test Product</h2>
+                            <p className="cart-table__prod-info__size">1 Gallon</p>
+                        </div>
+                    </td>
+                    <td className="cart-table__tbody__tr__td cart-table__price">$25</td>
+                    <td className="cart-table__tbody__tr__td cart-table__qty-container">
+                        <button>-</button>
+                        <span>2</span>
+                        <button>+</button>
+                    </td>
+                    <td className="cart-table__tbody__tr__td cart-table__total">$50</td>
+                </tr>
+                <tr className="cart-table__tbody__tr">
+                    <td className="cart-table__tbody__tr__td cart-table__prod-info">
+                        <figure className="cart-table__prod-info__fig">
+                            img here
+                        </figure>
+                        <div className="cart-table__prod-info__desc-container">
+                            <h2 className="cart-table__prod-info__title">Test Product 2</h2>
+                            <p className="cart-table__prod-info__size">1 Gallon</p>
+                        </div>
+                    </td>
+                    <td className="cart-table__tbody__tr__td cart-table__price">$20</td>
+                    <td className="cart-table__tbody__tr__td cart-table__qty-container">
+                        <button>-</button>
+                        <span>1</span>
+                        <button>+</button>
+                    </td>
+                    <td className="cart-table__tbody__tr__td cart-table__total">$20</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default CartTable;
