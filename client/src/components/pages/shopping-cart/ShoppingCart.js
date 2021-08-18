080c95e6d26ab61a5164d156792f0aab56826f5d
Alex Curran <alexcurran.va@gmail.com>
2021-08-18 13:38:37 -0500

    Dynamic data in for shopping cart page, remove hard-coded data

--- client/src/components/pages/shopping-cart/ShoppingCart.js
+++ client/src/components/pages/shopping-cart/ShoppingCart.js
@@ -3,13 +3,18 @@ import TitleBar from "../../ui/title-bar/TitleBar";
 import MainWrapper from "../../layout/main-wrapper/MainWrapper";
 import CartTable from "./cart-table/CartTable";
 
+import useCartStore from "../../../stores/CartStore";
+
 function ShoppingCart() {
+    const items = useCartStore((state) => state.items);
+    console.log(items);
+
     return (
         <div className="cart-page">
             <Header />
             <MainWrapper>
                 <TitleBar>Shopping Cart</TitleBar>
-                <CartTable />
+                <CartTable items={items} />
             </MainWrapper>
         </div>
     );
--- client/src/components/pages/shopping-cart/cart-table/CartTable.js
+++ client/src/components/pages/shopping-cart/cart-table/CartTable.js
@@ -2,7 +2,30 @@ import { Image, Transformation } from "cloudinary-react";
 
 import "./CartTable.css";
 
-function CartTable() {
+function CartTable({ items }) {
+    const itemsArr = items.map(item => (
+        <tr key={item.productId} className="cart-table__tbody__tr">
+            <td className="cart-table__tbody__tr__td cart-table__prod-info">
+                <figure className="cart-table__prod-info__fig">
+                    <Image publicId={item.imgPublicId} width="125" height="150" alt={item.imgAltTxt} className="cart-table__prod-info__fig__img">
+                        <Transformation width="125" height="150" crop="fill" quality="auto" fetchFormat="auto" />
+                    </Image>
+                </figure>
+                <div className="cart-table__prod-info__desc-container">
+                    <h2 className="cart-table__prod-info__title">{item.title}</h2>
+                    <p className="cart-table__prod-info__size">{item.size} Gallon</p>
+                </div>
+            </td>
+            <td className="cart-table__tbody__tr__td cart-table__price">${item.price}</td>
+            <td className="cart-table__tbody__tr__td cart-table__qty-container">
+                <button className="cart-table__qty-container__btn">-</button>
+                <span>{item.itemQuantity}</span>
+                <button className="cart-table__qty-container__btn">+</button>
+            </td>
+            <td className="cart-table__tbody__tr__td cart-table__total">${item.itemTotalPrice}</td>
+        </tr>
+    ));
+
     return (
         <table className="cart-table">
             <thead className="cart-table__thead">
@@ -14,46 +37,7 @@ function CartTable() {
                 </tr>
             </thead>
             <tbody className="cart-table__tbody">
-                <tr className="cart-table__tbody__tr">
-                    <td className="cart-table__tbody__tr__td cart-table__prod-info">
-                        <figure className="cart-table__prod-info__fig">
-                            <Image publicId="evergreen-app/yfbvbevvnqhy7932vzdr.jpg" width="125" height="150" alt="Plant" className="cart-table__prod-info__fig__img">
-                                <Transformation width="125" height="150" crop="fill" quality="auto" fetchFormat="auto" />
-                            </Image>
-                        </figure>
-                        <div className="cart-table__prod-info__desc-container">
-                            <h2 className="cart-table__prod-info__title">Test Product</h2>
-                            <p className="cart-table__prod-info__size">1 Gallon</p>
-                        </div>
-                    </td>
-                    <td className="cart-table__tbody__tr__td cart-table__price">$25</td>
-                    <td className="cart-table__tbody__tr__td cart-table__qty-container">
-                        <button className="cart-table__qty-container__btn">-</button>
-                        <span>2</span>
-                        <button className="cart-table__qty-container__btn">+</button>
-                    </td>
-                    <td className="cart-table__tbody__tr__td cart-table__total">$50</td>
-                </tr>
-                <tr className="cart-table__tbody__tr">
-                    <td className="cart-table__tbody__tr__td cart-table__prod-info">
-                        <figure className="cart-table__prod-info__fig">
-                            <Image publicId="evergreen-app/yfbvbevvnqhy7932vzdr.jpg" width="125" height="150" alt="Plant" className="cart-table__prod-info__fig__img">
-                                <Transformation width="125" height="150" crop="fill" quality="auto" fetchFormat="auto" />
-                            </Image>
-                        </figure>
-                        <div className="cart-table__prod-info__desc-container">
-                            <h2 className="cart-table__prod-info__title">Test Product 2</h2>
-                            <p className="cart-table__prod-info__size">1 Gallon</p>
-                        </div>
-                    </td>
-                    <td className="cart-table__tbody__tr__td cart-table__price">$20</td>
-                    <td className="cart-table__tbody__tr__td cart-table__qty-container">
-                        <button className="cart-table__qty-container__btn">-</button>
-                        <span>1</span>
-                        <button className="cart-table__qty-container__btn">+</button>
-                    </td>
-                    <td className="cart-table__tbody__tr__td cart-table__total">$20</td>
-                </tr>
+                {itemsArr}
             </tbody>
         </table>
     );
