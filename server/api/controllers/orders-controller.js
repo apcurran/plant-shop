970f509afc18af205ca8195afd3b0b3fcd999684
Alex Curran <alexcurran.va@gmail.com>
2021-08-20 14:54:17 -0500

    Working on item info retrieval from db

--- server/api/controllers/orders-controller.js
+++ server/api/controllers/orders-controller.js
@@ -5,9 +5,35 @@ const jwt = require("jsonwebtoken");
 const db = require("../../db/index");
 
 async function postCreatePaymentIntent(req, res, next) {
+    const { currItemsArr, totalQty } = req.body;
+    console.log(req.body);
+    const client = await db.pool
+                            .connect()
+                            .catch((err) => next(err));
+
+    let itemsInfoFromDb = [];
+
     try {
-        console.log(req.body);
-        res.end();
+        for (let itemObj of currItemsArr) {
+            // NOT WORKING BUG
+            const itemInfo = (await db.query(`
+                SELECT
+                    product.title,
+                    product_extra_info.size,
+                    product_extra_info.price
+                FROM product
+                INNER JOIN product_extra_info
+                    ON product.product_id = product_extra_info.product_id
+                WHERE
+                    product.product_id = $1
+                    AND
+                    product_extra_info.product_extra_info_id = $2
+            `, [itemObj.product_id, itemObj.productExtraInfoId])).rows;
+
+            itemsInfoFromDb.push(itemInfo);
+        }
+
+        console.log(itemsInfoFromDb);
 
     } catch (err) {
         next(err);
