67d6f102ccb38fa1dc4e0c90647fbb285d28a309
Alex Curran <alexcurran.va@gmail.com>
2021-08-20 15:07:06 -0500

    Data coming in correctly for product info in controller

--- server/api/controllers/orders-controller.js
+++ server/api/controllers/orders-controller.js
@@ -6,34 +6,31 @@ const db = require("../../db/index");
 
 async function postCreatePaymentIntent(req, res, next) {
     const { currItemsArr, totalQty } = req.body;
-    console.log(req.body);
-    const client = await db.pool
-                            .connect()
-                            .catch((err) => next(err));
-
     let itemsInfoFromDb = [];
 
     try {
         for (let itemObj of currItemsArr) {
-            // NOT WORKING BUG
+            const prodId = Number(itemObj.productId);
+            const productExtraInfoId = itemObj.productExtraInfoId;
             const itemInfo = (await db.query(`
                 SELECT
                     product.title,
                     product_extra_info.size,
                     product_extra_info.price
                 FROM product
-                INNER JOIN product_extra_info
-                    ON product.product_id = product_extra_info.product_id
+                INNER JOIN
+                    product_extra_info ON product.product_id = product_extra_info.product_id
                 WHERE
                     product.product_id = $1
                     AND
                     product_extra_info.product_extra_info_id = $2
-            `, [itemObj.product_id, itemObj.productExtraInfoId])).rows;
+            `, [prodId, productExtraInfoId])).rows;
 
             itemsInfoFromDb.push(itemInfo);
         }
 
         console.log(itemsInfoFromDb);
+        // NOTE: Data coming in correctly
 
     } catch (err) {
         next(err);
