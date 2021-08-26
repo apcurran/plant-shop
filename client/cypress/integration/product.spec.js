6821872d86b76042e081c08229ca4f2a43c034a5
Alex <alexcurran.va@gmail.com>
2021-08-26 14:39:37 -0500

    Finish product add to cart flow

--- client/cypress/integration/product.spec.js
+++ client/cypress/integration/product.spec.js
@@ -8,5 +8,11 @@ describe("product page", () => {
     it("should update cart icon total qty when a product is added to the cart", () => {
         cy.contains("button", "1 gallon")
           .click();
+
+        cy.contains("button", /add to cart/i)
+          .click();
+
+        cy.get(".nav__cart-btn__item-qty")
+          .should("not.have.text", "0");
     });
 });
