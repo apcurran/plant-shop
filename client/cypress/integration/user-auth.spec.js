c3281f0019cba2f110f9b54bafa07ce02a62a229
Alex <alexcurran.va@gmail.com>
2021-08-26 15:27:55 -0500

    Log out user test

--- client/cypress/integration/user-auth.spec.js
+++ client/cypress/integration/user-auth.spec.js
@@ -19,3 +19,25 @@ describe("log in flow", () => {
           .should("be.visible");
     });
 });
+
+describe("user log out", () => {
+    beforeEach(() => {
+        cy.visit("/auth/log-in");
+    });
+
+    it("should log out John Doe user", () => {
+        cy.get("#email")
+          .type("johndoe@gmail.com");
+
+        cy.get("#password")
+          .type("password");
+
+        cy.contains("button", /submit/i)
+          .click();
+
+        // User now logged in
+
+        cy.contains("button", /log out/i)
+          .click();
+    });
+});
