86a8f76db957f43980a50f601c4ce1e4599cd408
Alex <alexcurran.va@gmail.com>
2021-08-26 14:30:39 -0500

    First test for product page

--- client/cypress/integration/product.spec.js
+++ client/cypress/integration/product.spec.js
@@ -6,6 +6,7 @@ describe("product page", () => {
     });
 
     it("should update cart icon total qty when a product is added to the cart", () => {
-        
+        cy.contains("button", "1 gallon")
+          .click();
     });
 });
