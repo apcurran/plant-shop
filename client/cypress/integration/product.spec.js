2275b82d2e568070171d26469bc34e5cb4c3882d
Alex <alexcurran.va@gmail.com>
2021-08-26 14:23:36 -0500

    Cypress testing lib setup

---
+++ client/cypress/integration/product.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("product page", () => {
+    beforeEach(() => {
+        cy.visit("/collections/4");
+    });
+
+    it("should update cart icon total qty when a product is added to the cart", () => {
+        
+    });
+});
