268a22fc9e63c289b8238e84d7424a53276932dd
Alex <alexcurran.va@gmail.com>
2021-08-27 12:01:08 -0500

    Stubbed API req test for user sign-up flow

--- client/cypress/integration/user-auth.spec.js
+++ client/cypress/integration/user-auth.spec.js
@@ -41,3 +41,35 @@ describe("user log out", () => {
           .click();
     });
 });
+
+describe("user sign up", () => {
+  // Mocked req to server API
+  beforeEach(() => {
+    cy.visit("/auth/sign-up");
+  });
+
+  it("should sign up a user as John Doe", () => {
+    // Stubbed API req for user sign-up
+    cy.intercept("POST", "/api/auth/sign-up", {
+      statusCode: 201,
+      body: {
+        message: "New user created"
+      }
+    })
+
+    cy.get("#first-name")
+      .type("John");
+
+    cy.get("#last-name")
+      .type("Doe");
+
+    cy.get("#email")
+      .type("johndoe@gmail.com");
+
+    cy.get("#password")
+      .type("password");
+
+    cy.contains("button", "Submit")
+      .click();
+  });
+})
