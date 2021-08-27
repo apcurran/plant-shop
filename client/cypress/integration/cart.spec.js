831dfa59fdadda4b614997411b7af70602db20de
Alex <alexcurran.va@gmail.com>
2021-08-27 09:39:57 -0500

    Cart base functionality

---
+++ client/cypress/integration/cart.spec.js
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+
+describe("cart functionality", () => {
+    beforeEach(() => {
+        cy.visit("/collections/4");
+
+        sessionStorage.clear();
+
+        cy.contains("button", "1 gallon")
+          .click();
+
+        cy.contains("button", /add to cart/i)
+          .click();
+
+        cy.visit("/cart");
+    });
+
+    it("product should be in cart table", () => {
+        cy.contains("h2", /the pothos plant/i)
+          .should("have.text", "The Pothos Plant");
+    });
+    
+    it("product should contain a price of $15", () => {
+        cy.get(".cart-table__price")
+        .should("have.text", "$15");
+    });
+    
+    it("product should contain a total of $15", () => {
+        cy.get(".cart-table__total")
+        .should("have.text", "$15");
+    });
+    
+    it("product should have a qty of 1", () => {
+        cy.get(".cart-table__qty-container span")
+          .should("have.text", "1");
+    });
+
+    it("product qty should increment by 1 to make a total product qty of 2", () => {
+        cy.contains("button", "+")
+          .click();
+
+        cy.get(".cart-table__qty-container span")
+          .should("have.text", "2");
+    });
+});
