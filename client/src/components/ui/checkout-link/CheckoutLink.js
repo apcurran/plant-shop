9bd8ce28a7c1b9e783ae8490ab8e806c7b68de9f
Alex Curran <alexcurran.va@gmail.com>
2021-08-30 11:20:27 -0500

    Apply styles to CheckoutLink comp

--- client/src/components/ui/checkout-link/CheckoutLink.js
+++ client/src/components/ui/checkout-link/CheckoutLink.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 
+import "./CheckoutLink.css";
+
 function CheckoutLink() {
     return (
-        <Link to="/checkout">Checkout</Link>
+        <Link to="/checkout" className="checkout-link">Checkout</Link>
     );
 }
 
