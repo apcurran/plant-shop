c08dee801a7ef869e3586e91fac5342450009c82
Alex Curran <alexcurran.va@gmail.com>
2021-08-30 16:09:00 -0500

    Shipping grid content section info

--- client/src/components/pages/checkout/shipping-content/ShippingContent.js
+++ client/src/components/pages/checkout/shipping-content/ShippingContent.js
@@ -1,9 +1,17 @@
+import { Image, Transformation } from "cloudinary-react";
+
 import "./ShippingContent.css";
 
 function ShippingContent() {
     return (
-        <div>
-            Shipping content
+        <div className="shipping-grid__content">
+            <h2 className="shipping-grid__content__title">Shipping Info</h2>
+            <p className="shipping-grid__content__desc">Enter cardholder's name and address information.</p>
+            <figure className="shipping-grid__content__fig">
+                <Image className="shipping-grid__content__fig__img" publicId="evergreen-app/shipping-details/shipping_yor51n.png" width="1600" height="1200">
+                    <Transformation width="600" crop="fill" quality="auto" fetchFormat="auto" />
+                </Image>
+            </figure>
         </div>
     );
 }
