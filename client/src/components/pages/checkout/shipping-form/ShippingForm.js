0877d9df9926667f0a04b1e1b752885fe7ed011e
Alex Curran <alexcurran.va@gmail.com>
2021-08-30 16:28:09 -0500

    Base form styling/layout

--- client/src/components/pages/checkout/shipping-form/ShippingForm.js
+++ client/src/components/pages/checkout/shipping-form/ShippingForm.js
@@ -2,9 +2,32 @@ import "./ShippingForm.css";
 
 function ShippingForm() {
     return (
-        <div>
-            Shipping form
-        </div>
+        <form className="shipping-grid__form">
+            <div className="shipping-grid__form-group">
+                <label htmlFor="first-name" className="shipping-grid__form-group__label">First Name</label>
+                <input type="text" className="shipping-grid__form-group__input" id="first-name" />
+            </div>
+            <div className="shipping-grid__form-group">
+                <label htmlFor="last-name" className="shipping-grid__form-group__label">Last Name</label>
+                <input type="text" className="shipping-grid__form-group__input" id="last-name" />
+            </div>
+            <div className="shipping-grid__form-group">
+                <label htmlFor="street" className="shipping-grid__form-group__label">Street Address</label>
+                <input type="text" className="shipping-grid__form-group__input shipping-grid__form-group__input--long" id="street" />
+            </div>
+            <div className="shipping-grid__form-group">
+                <label htmlFor="city" className="shipping-grid__form-group__label">City</label>
+                <input type="text" className="shipping-grid__form-group__input" id="city" />
+            </div>
+            <div className="shipping-grid__form-group">
+                <label htmlFor="state" className="shipping-grid__form-group__label">State</label>
+                <input type="text" className="shipping-grid__form-group__input" id="state" />
+            </div>
+            <div className="shipping-grid__form-group">
+                <label htmlFor="zip" className="shipping-grid__form-group__label">Zip Code</label>
+                <input type="text" className="shipping-grid__form-group__input" id="zip" />
+            </div>
+        </form>
     );
 }
 
