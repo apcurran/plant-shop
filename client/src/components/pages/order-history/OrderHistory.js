793b221f745031c1253fa875b7487433e61ff42a
Alex Curran <alexcurran.va@gmail.com>
2021-09-09 11:42:21 -0500

    Data fetch with error handling for OrderHistory comp

--- client/src/components/pages/order-history/OrderHistory.js
+++ client/src/components/pages/order-history/OrderHistory.js
@@ -1,14 +1,66 @@
+import { useEffect, useState, useCallback } from "react";
+
+import useAuthStore from "../../../stores/AuthStore";
+
 import Header from "../../layout/header/Header";
 import MainWrapper from "../../layout/main-wrapper/MainWrapper";
 import TitleBar from "../../ui/title-bar/TitleBar";
+import ErrorMsg from "../../ui/error-msg/ErrorMsg";
+import LoadingSpinner from "../../ui/loading-spinner/LoadingSpinner";
 
 function OrderHistory() {
+    const [orders, setOrders] = useState([]);
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    // Global store state
+    const token = useAuthStore(
+        useCallback((state) => state.token, [])
+    );
+
+    useEffect(() => {
+        async function getOrders() {
+            setIsLoading(true);
+
+            try {
+                const response = await fetch("/api/orders/order-history", {
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    }
+                });
+
+                // Check for errors
+                if (!response.ok) {
+                    const serverErrMsg = await response.json();
+
+                    throw Error(serverErrMsg.error);
+                }
+
+                setIsLoading(false);
+
+                const data = await response.json();
+                setOrders(data);
+
+            } catch (err) {
+                setIsLoading(false);
+                setError(err);
+            }
+        }
+
+        if (token) getOrders();
+    }, [token]);
+
+    // JSX elems
+    const errorMsg = error ? <ErrorMsg error={error} /> : null;
+    const loadingSpinner = isLoading ? <LoadingSpinner /> : null;
+
     return (
         <div className="order-history">
             <Header />
             <MainWrapper>
                 <TitleBar>Order History</TitleBar>
-                
+                {errorMsg}
+                {loadingSpinner}
             </MainWrapper>
         </div>
     );
