487a3416fa9d163fd58693621e20ac8af458cf39
Alex Curran <alexcurran.va@gmail.com>
2021-09-10 10:04:01 -0500

    Cloudinary imgs outputting correctly to order history page

--- client/src/components/pages/order-history/order-item/OrderItem.js
+++ client/src/components/pages/order-history/order-item/OrderItem.js
@@ -1,9 +1,20 @@
+import { Image, Transformation } from "cloudinary-react";
+
 import "./OrderItem.css";
 
 function OrderItem({ title, category, price, size, qty, publicId, altText, width, height }) {
     return (
         <div className="order-item">
-            Order Item
+            <div className="order-item__inner-container">
+                <figure className="order-item__fig">
+                    <Image className="order-item__fig__img" publicId={publicId} width={width} height={height} alt={altText}>
+                        <Transformation width="150" crop="fill" quality="auto" fetchFormat="auto" />
+                    </Image>
+                </figure>
+                <div className="order-item__content">
+                    
+                </div>
+            </div>
         </div>
     );
 }
