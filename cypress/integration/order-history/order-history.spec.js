d7b89c4ecca7459d063de732a304965847f1e028
Alex Curran <alexcurran.va@gmail.com>
2022-02-24 14:34:22 -0600

    Adjustment to test desc

--- cypress/integration/order-history/order-history.spec.js
+++ cypress/integration/order-history/order-history.spec.js
@@ -16,12 +16,15 @@ describe("order history", () => {
             .click();
     });
 
-    it("should display an order with 1 oak tree, 2 ash trees, and one pear tree", () => {
+    it("should display an order with 1 oak tree, 2 ash trees, and 1 pear tree", () => {
         cy.contains("a", "Your Orders")
             .click();
 
         // Order date
         cy.contains("February 24, 2022").should("be.visible");
+
+        cy.get(".order-item")
+            .should("have.length", 3);
 
         cy.get(".order-item")
             .contains("The Oak Tree");
