fad3c3656d675e0bd4dfc376e02d0ff641a3e7b0
Alex Curran <alexcurran.va@gmail.com>
2022-02-24 09:35:11 -0600

    Add test for wrong user email during log-in

--- client/cypress/integration/auth/log-in.spec.js
+++ client/cypress/integration/auth/log-in.spec.js
@@ -1,9 +1,11 @@
 /// <reference types="cypress" />
 
 describe("log in flow", () => {
-    it("should log in user as John Doe", () => {
+    beforeEach(() => {
         cy.visit("/auth/log-in");
+    });
 
+    it("should log in user as John Doe", () => {
         cy.get("#email")
             .type("johndoe@gmail.com");
 
@@ -14,6 +16,20 @@ describe("log in flow", () => {
             .click();
 
         cy.contains("header", /hello, john/i)
+            .should("be.visible");
+    });
+
+    it("should give error message if user email does not exist in db", () => {
+        cy.get("#email")
+            .type("fakeemail@gmail.com");
+
+        cy.get("#password")
+            .type("fakepassword");
+
+        cy.contains("button", /submit/i)
+            .click();
+
+        cy.contains("p", /Email is not found/i)
             .should("be.visible");
     });
 });
