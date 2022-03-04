b98f5b4b9a0974bd712061e9ea96e6c5e7595237
Alex Curran <alexcurran.va@gmail.com>
2022-03-04 13:54:13 -0600

    Test for add new product functionality

--- cypress/integration/admin/admin-new-product.spec.js
+++ cypress/integration/admin/admin-new-product.spec.js
@@ -5,7 +5,60 @@ describe("admin add new product", () => {
         cy.login("admin");
     });
 
-    it("new product information should be filled in and created", () => {
-        
+    it("navigate to add product page, fill out form data, and submit", () => {
+        // stub API req
+        cy.intercept("POST", "/api/products", {
+            statusCode: 201,
+            body: {
+                message: "Product information added."
+            }
+        });
+
+        cy.contains("a", /add product/i)
+            .should("exist")
+            .click();
+
+        cy.contains("h2", "Product Info")
+            .should("exist");
+
+        cy.get("input[id=title]")
+            .type("The Pomegranate Tree");
+
+        cy.get("textarea[id=description]")
+            .type("Lorem ipsum");
+
+        cy.get("select[id=category]")
+            .select("fruit trees");
+
+        cy.get("input[id=size-small-amt]")
+            .type("1");
+
+        cy.get("input[id=size-small-price]")
+            .type("15");
+
+        cy.get("input[id=size-medium-amt]")
+            .type("2");
+
+        cy.get("input[id=size-medium-price]")
+            .type("20");
+
+        cy.get("input[id=size-large-amt]")
+            .type("3");
+
+        cy.get("input[id=size-large-price]")
+            .type("25");
+
+        cy.fixture("images/pomegranate-tree.jpg")
+            .as("pomTree");
+
+        cy.get("input[id=img-file]")
+            .selectFile("@pomTree");
+
+        cy.get("textarea[id=img-alt-txt]")
+            .type("A sliced pomegranate sitting on a stack of other pomegranates.");
+
+        cy.contains("button", /submit/i)
+            .should("exist")
+            .click();
     });
 });
