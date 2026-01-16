567698c00398fd614da316553bced330ecd81dd4
Alex Curran <alexcurran.va@gmail.com>
2026-01-16 15:47:43 -0600

    Replace `dotenv` dep with native Node.js env file support in scripts of package.json

--- db/index.js
+++ db/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-require("dotenv").config();
-
 const pgp = require("pg-promise")({ capSQL: true });
 
 const db = pgp({
--- util/send-email.js
+++ util/send-email.js
@@ -1,7 +1,5 @@
 "use strict";
 
-require("dotenv").config();
-
 const nodemailer = require("nodemailer");
 
 async function sendResetLink(id, userEmail) {
