bbe7faf1b1ef484cf0a83bdb4d4307047d520824
Alex Curran <alexcurran.va@gmail.com>
2026-01-19 16:31:46 -0600

    Explicitly define algo for jsonwebtoken

--- api/middleware/verify-admin.js
+++ api/middleware/verify-admin.js
@@ -10,21 +10,26 @@ function verifyAdmin(req, res, next) {
         return res.status(401).json({ error: "Access denied." });
     }
 
-    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-        if (err) {
-            console.error(err);
+    jwt.verify(
+        token,
+        process.env.TOKEN_SECRET,
+        { algorithms: ["HS256"] },
+        (err, user) => {
+            if (err) {
+                console.error(err);
 
-            return res.status(403).json({ error: "Invalid token." });
-        }
+                return res.status(403).json({ error: "Invalid token." });
+            }
 
-        if (!user.isAdmin) {
-            return res.status(403).json({ error: "Unauthorized access." });
-        }
+            if (!user.isAdmin) {
+                return res.status(403).json({ error: "Unauthorized access." });
+            }
 
-        // Validation passed
-        req.user = user;
-        next();
-    });
+            // Validation passed
+            req.user = user;
+            next();
+        },
+    );
 }
 
 module.exports = { verifyAdmin };
--- api/middleware/verify-auth.js
+++ api/middleware/verify-auth.js
@@ -10,17 +10,22 @@ function verifyAuth(req, res, next) {
         return res.status(401).json({ error: "Access denied." });
     }
 
-    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-        if (err) {
-            console.error(err);
+    jwt.verify(
+        token,
+        process.env.TOKEN_SECRET,
+        { algorithms: ["HS256"] },
+        (err, user) => {
+            if (err) {
+                console.error(err);
 
-            return res.status(403).json({ error: "Invalid token." });
-        }
+                return res.status(403).json({ error: "Invalid token." });
+            }
 
-        // Validation passed
-        req.user = user;
-        next();
-    });
+            // Validation passed
+            req.user = user;
+            next();
+        },
+    );
 }
 
 module.exports = { verifyAuth };
