e2c99699123fbad66d0eabf37a05251b531799ea
Alex Curran <alexcurran.va@gmail.com>
2026-05-08 13:30:01 -0500

    Refactor AuthImg for Cloudinary

--- client/src/components/pages/auth/auth-img/AuthImg.jsx
+++ client/src/components/pages/auth/auth-img/AuthImg.jsx
@@ -1,17 +1,24 @@
-import { Image, Transformation } from "cloudinary-react";
+import { AdvancedImage } from "@cloudinary/react";
+import { fill } from "@cloudinary/url-gen/actions/resize";
+
+import { cld } from "@utils/cloudinary-setup";
 
 import "./AuthImg.css";
 
 function AuthImg({ imgPublicId, imgWidth, imgHeight }) {
+    const img = cld
+        .image(imgPublicId)
+        .resize(fill())
+        .quality("auto")
+        .format("auto");
+
     return (
-        <Image
-            publicId={imgPublicId}
+        <AdvancedImage
+            cldImg={img}
             className="auth-card__fig__img sign-up__card__fig__img"
             width={imgWidth}
             height={imgHeight}
-        >
-            <Transformation fetchFormat="auto" quality="auto" />
-        </Image>
+        />
     );
 }
 
