d0c33b47b170a9217aa3dfb8b0561666f65ed0df
Alex Curran <alexcurran.va@gmail.com>
2026-05-08 13:34:47 -0500

    Refactor to new Cloudinary lib for ShippingContent component

--- client/src/components/pages/checkout/shipping-content/ShippingContent.jsx
+++ client/src/components/pages/checkout/shipping-content/ShippingContent.jsx
@@ -1,8 +1,18 @@
-import { Image, Transformation } from "cloudinary-react";
+// import { Image, Transformation } from "cloudinary-react";
+import { AdvancedImage } from "@cloudinary/react";
+
+import { cld } from "@utils/cloudinary-setup";
 
 import "./ShippingContent.css";
+import { fill } from "@cloudinary/url-gen/actions/resize";
 
 function ShippingContent() {
+    const img = cld
+        .image("evergreen-app/shipping-details/shipping_yor51n.png")
+        .resize(fill().width(600))
+        .quality("auto")
+        .format("auto");
+
     return (
         <div className="shipping-grid__content">
             <h2 className="shipping-grid__content__title">Shipping Info</h2>
@@ -10,19 +20,12 @@ function ShippingContent() {
                 Enter cardholder's name and address information.
             </p>
             <figure className="shipping-grid__content__fig">
-                <Image
+                <AdvancedImage
+                    cldImg={img}
                     className="shipping-grid__content__fig__img"
-                    publicId="evergreen-app/shipping-details/shipping_yor51n.png"
                     width="1600"
                     height="1200"
-                >
-                    <Transformation
-                        width="600"
-                        crop="fill"
-                        quality="auto"
-                        fetchFormat="auto"
-                    />
-                </Image>
+                />
             </figure>
         </div>
     );
