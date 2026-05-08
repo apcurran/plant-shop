bbf324e933569b5d59c9f222d81c4da247c7d81a
Alex Curran <alexcurran.va@gmail.com>
2026-05-08 16:02:25 -0500

    Refactor img for SuccessImg comp

--- client/src/components/pages/payment-success/success-img/SuccessImg.jsx
+++ client/src/components/pages/payment-success/success-img/SuccessImg.jsx
@@ -1,22 +1,25 @@
-import { Image, Transformation } from "cloudinary-react";
+import { AdvancedImage } from "@cloudinary/react";
+
+import { cld } from "@utils/cloudinary-setup";
 
 import "./SuccessImg.css";
+import { scale } from "@cloudinary/url-gen/actions/resize";
 
 function SuccessImg() {
+    const img = cld
+        .image("evergreen-app/payment-success/checkout_z65rza.png")
+        .resize(scale().width(1000))
+        .quality("auto")
+        .format("auto");
+
     return (
         <figure className="payment-success__fig">
-            <Image
+            <AdvancedImage
+                cldImg={img}
                 className="payment-success__fig__img"
                 width="1600"
                 height="1200"
-                publicId="evergreen-app/payment-success/checkout_z65rza.png"
-            >
-                <Transformation
-                    width="1000"
-                    quality="auto"
-                    fetchFormat="auto"
-                />
-            </Image>
+            />
         </figure>
     );
 }
