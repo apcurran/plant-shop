c09eed940881dc83c457db2fb9f16f3b6ce0f2ab
Alex Curran <alexcurran.va@gmail.com>
2026-05-20 11:58:45 -0500

    Refactor tests to use new cy.env() instead of old, deprecated Cypress.env() calls

--- cypress/e2e/auth/log-in.cy.js
+++ cypress/e2e/auth/log-in.cy.js
@@ -6,13 +6,15 @@ describe("log in flow", () => {
     });
 
     it("should log in user as Bob Doe", () => {
-        cy.get("#email").type(Cypress.env("testUserEmail"));
+        cy.env(["testUserEmail", "testUserPassword"]).then(
+            ({ testUserEmail, testUserPassword }) => {
+                cy.get("#email").type(testUserEmail);
+                cy.get("#password").type(testUserPassword);
 
-        cy.get("#password").type(Cypress.env("testUserPassword"));
-
-        cy.contains("button", /submit/i).click();
-
-        cy.contains("header", /hello, bob/i).should("be.visible");
+                cy.contains("button", /submit/i).click();
+                cy.contains("header", /hello, bob/i).should("be.visible");
+            },
+        );
     });
 
     it("should give error message if user email does not exist in db", () => {
@@ -26,28 +28,28 @@ describe("log in flow", () => {
     });
 
     it("should give error message if user provides an email shorter than 6 characters long", () => {
-        cy.get("#email").type(Cypress.env("testUserEmail"));
+        cy.env(["testUserEmail"]).then(({ testUserEmail }) => {
+            cy.get("#email").type(testUserEmail);
+            cy.get("#password").type("short");
 
-        cy.get("#password").type("short");
-
-        cy.contains("button", /submit/i).click();
-
-        cy.contains(
-            "p",
-            /"password" length must be at least 6 characters long/i,
-        ).should("be.visible");
+            cy.contains("button", /submit/i).click();
+            cy.contains(
+                "p",
+                /"password" length must be at least 6 characters long/i,
+            ).should("be.visible");
+        });
     });
 
     it("should give error message if user provides a password that is incorrect", () => {
-        cy.get("#email").type(Cypress.env("testUserEmail"));
+        cy.env(["testUserEmail"]).then(({ testUserEmail }) => {
+            cy.get("#email").type(testUserEmail);
+            cy.get("#password").type("fakepassword");
 
-        cy.get("#password").type("fakepassword");
-
-        cy.contains("button", /submit/i).click();
-
-        cy.contains(
-            "p",
-            /Invalid credentials provided. Check your email or password again./i,
-        ).should("be.visible");
+            cy.contains("button", /submit/i).click();
+            cy.contains(
+                "p",
+                /Invalid credentials provided. Check your email or password again./i,
+            ).should("be.visible");
+        });
     });
 });
