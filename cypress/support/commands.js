4e08ff0584c4ce78d682a646ee7c7c3aaf588f98
Alex Curran <alexcurran.va@gmail.com>
2026-05-20 12:04:26 -0500

    Refactor to cy.env() call

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,32 +1,40 @@
 /// <reference types="cypress" />
 
 Cypress.Commands.add("login", (type) => {
-    // dual log in functionality for either a user or admin
-    const email =
-        type === "admin"
-            ? Cypress.env("adminEmail")
-            : Cypress.env("testUserEmail");
-    const password =
-        type === "admin"
-            ? Cypress.env("adminPassword")
-            : Cypress.env("testUserPassword");
+    // Request all required environment variables asynchronously
+    cy.env([
+        "adminEmail",
+        "testUserEmail",
+        "adminPassword",
+        "testUserPassword",
+    ]).then(
+        ({ adminEmail, testUserEmail, adminPassword, testUserPassword }) => {
+            // Determine credentials inside the asynchronous callback
+            const email = type === "admin" ? adminEmail : testUserEmail;
+            const password =
+                type === "admin" ? adminPassword : testUserPassword;
 
-    cy.request({
-        method: "POST",
-        url: "http://localhost:3000/api/auth/log-in",
-        body: {
-            email: email,
-            password: password,
+            cy.request({
+                method: "POST",
+                url: "http://localhost:3000/api/auth/log-in",
+                body: {
+                    email: email,
+                    password: password,
+                },
+            }).then((response) => {
+                const { accessToken, userInfo } = response.body;
+                const { isAdmin } = userInfo;
+
+                window.sessionStorage.setItem("accessToken", accessToken);
+                window.sessionStorage.setItem(
+                    "userInfo",
+                    JSON.stringify(userInfo),
+                );
+                window.sessionStorage.setItem("isAdmin", isAdmin);
+
+                // redirect user after log in
+                cy.visit("/collections");
+            });
         },
-    }).then((response) => {
-        const { accessToken, userInfo } = response.body;
-        const { isAdmin } = userInfo;
-
-        window.sessionStorage.setItem("accessToken", accessToken);
-        window.sessionStorage.setItem("userInfo", JSON.stringify(userInfo));
-        window.sessionStorage.setItem("isAdmin", isAdmin);
-
-        // redirect user after log in
-        cy.visit("/collections");
-    });
+    );
 });
