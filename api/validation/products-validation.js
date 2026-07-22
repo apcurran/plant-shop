707bed8c440b76a30a9fe280d91fd940c5789e2d
Alex Curran <alexcurran.va@gmail.com>
2026-07-22 17:01:26 -0500

    Pre-compile schemas, prevent re-gen on endpoint req

--- api/validation/products-validation.js
+++ api/validation/products-validation.js
@@ -1,27 +1,27 @@
 import Joi from "joi";
 
-export function postProductValidation(data) {
-    const schema = Joi.object({
-        title: Joi.string().trim().required(),
-        description: Joi.string().trim().required(),
-        category: Joi.string().trim().required(),
-        productExtraInfo: Joi.string() // Stringified array of data
-            .required(),
-        imgAltText: Joi.string().trim().required(),
-    });
+const createProductSchema = Joi.object({
+    title: Joi.string().trim().required(),
+    description: Joi.string().trim().required(),
+    category: Joi.string().trim().required(),
+    productExtraInfo: Joi.string() // Stringified array of data
+        .required(),
+    imgAltText: Joi.string().trim().required(),
+});
 
-    return schema.validateAsync(data);
+const updateProductSchema = Joi.object({
+    title: Joi.string().trim(),
+    description: Joi.string().trim(),
+    category: Joi.string().trim(),
+    productExtraInfo: Joi.string(), // Stringified array of data
+    productImg: Joi.string(), // Possible 'null' str val
+    imgAltText: Joi.string().trim(),
+});
+
+export function postProductValidation(data) {
+    return createProductSchema.validateAsync(data);
 }
 
 export function patchProductValidation(data) {
-    const schema = Joi.object({
-        title: Joi.string().trim(),
-        description: Joi.string().trim(),
-        category: Joi.string().trim(),
-        productExtraInfo: Joi.string(), // Stringified array of data
-        productImg: Joi.string(), // Possible 'null' str val
-        imgAltText: Joi.string().trim(),
-    });
-
-    return schema.validateAsync(data);
+    return updateProductSchema.validateAsync(data);
 }
